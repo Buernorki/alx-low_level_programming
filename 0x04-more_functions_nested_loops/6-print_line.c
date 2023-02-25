43f9b0f4071896241410d951e8d39978f78ae8e7
Buernorki <122885608+Buernorki@users.noreply.github.com>
2023-02-25 11:20:49 +0000

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_line - function that checks for uppercase character.
+ * print_line - draws a straight line.
  * @n: is the integer for the paramaters of my function
  * Return: 0
  */
