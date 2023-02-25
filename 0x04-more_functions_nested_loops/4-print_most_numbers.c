8bda8299a55adc005e357617984bab0db9bcada1
Buernorki <122885608+Buernorki@users.noreply.github.com>
2023-02-25 04:59:10 +0400

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,17 @@
  */
 void print_most_numbers(void)
 {
-	char c;
+int c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+for (c = 48; c < 58; c++)
+{
+if (c != 50)
+{
+if (c != 52)
+{
+_putchar(c);
+}
+}
+}
+_putchar('\n');
 }
