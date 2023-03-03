b4359b6d30dfe896ace189a258d11c2f8e456957
Buernorki <122885608+Buernorki@users.noreply.github.com>
2023-03-03 20:55:30 +0000

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,11 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _strcat -  appends the src string to the dest string,
- * overwriting the terminating null byte (\0) at the end of dest,
- * and then adds a terminating null byte
- *@dest: The destination string
- *@src: The source string
- *Return: Apointer to the resulting string desk
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int i;
@@ -31,4 +27,3 @@ char *_strcat(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
-
