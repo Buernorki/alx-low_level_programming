1201bcd760d8ee48e28f4840ece41fe6b8ce5595
Buernorki <buerki19@gmail.com>
2023-03-15 17:10:25 -0700

    update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,13 +2,16 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
+ * Return: void
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+
+	while (str[i])
 	{
-		_putchar(*str++);
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
-
